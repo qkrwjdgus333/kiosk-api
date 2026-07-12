c774514f24a8fe3507e37d9d6187484182bec38d
qkrwjdgus333 <pqofjf999@gmail.com>
2026-07-12 22:18:01 +0900

    feat: making router fun for get_schedule

--- app/domain/schedule/router.py
+++ app/domain/schedule/router.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Path
 from sqlalchemy.orm import Session
 
 
 from app.database import get_db
-from app.domain.schedule.schema import ScheduleCreateRequest, ScheduleCreateResponse
+from app.domain.schedule.schema import ScheduleCreateRequest, ScheduleCreateResponse, ScheduleDetailResponse
 from app.domain.schedule import service
 from app.core.exceptions import BusinessException
 
@@ -21,3 +21,12 @@ def create_schedule(request: ScheduleCreateRequest, db: Session = Depends(get_db
             status_code=status.HTTP_400_BAD_REQUEST,
             detail={"code": "DOMAIN_VALIDATION_ERROR", "message": str(e)}
         )
+
+def get_schedule(id: int = Path(..., description="조회하려는 일정의 ID"),
+                 db: Session = Depends(get_db)):
+    try:
+        return service.get_schedule(db, id)
+    except BusinessException as e:
+        raise HTTPException(status_code=e.status_code,
+                            detail={"code": e.code, "message": e.message}
+                            )
