3da9da681e5999038442c0ffd5c81db55b840c67
qkrwjdgus333 <pqofjf999@gmail.com>
2026-07-14 20:02:46 +0900

    feat: add update router

--- app/domain/schedule/router.py
+++ app/domain/schedule/router.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Path
 from sqlalchemy.orm import Session
 
 
 from app.database import get_db
-from app.domain.schedule.schema import ScheduleCreateRequest, ScheduleCreateResponse
+from app.domain.schedule.schema import *
 from app.domain.schedule import service
 from app.core.exceptions import BusinessException
 
@@ -20,4 +20,19 @@ def create_schedule(request: ScheduleCreateRequest, db: Session = Depends(get_db
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail={"code": "DOMAIN_VALIDATION_ERROR", "message": str(e)}
+        )
+
+@router.patch("/{id}", response_model=ScheduleDetailResponse, status_code=status.HTTP_200_OK)
+def update_schedule(
+    request: ScheduleUpdateRequest,
+    id: int = Path(...),
+    db: Session = Depends(get_db)
+):
+    
+    try:
+        return service.update_schedule(db=db, schedule_id=id, request=request)
+    except BusinessException as e:
+        raise HTTPException(
+            status_code=e.status_code,
+            detail={"code": e.code, "message": e.message}
         )
